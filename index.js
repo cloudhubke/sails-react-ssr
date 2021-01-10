f59832f9893e64bcc5f7118f8d447544cf1d80d2
Bernard Gaitho <gaithoben@gmail.com>
2021-01-10 14:20:48 +0300

    updates

--- index.js
+++ index.js
@@ -62,6 +62,17 @@ register(undefined, (mod, filename) => {
 require("@babel/register")({
   ignore: [/(node_modules)/],
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: ["@babel/plugin-syntax-dynamic-import", "react-loadable/babel"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          models: "./models",
+        },
+      },
+    ],
+    "@babel/plugin-syntax-dynamic-import",
+    "react-loadable/babel",
+  ],
 });
 // require('./index');
